6d1ef71a3cb87812bb514a605c50bc31d6f34db5
Pedro Henrique Vieira Fernandes <72428350+PedrohvFernandes@users.noreply.github.com>
2022-05-07 17:58:53 -0300

    add port env variable

--- server/src/server.ts
+++ server/src/server.ts
@@ -9,6 +9,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routes);
-app.listen(3333, () => {
+
+// Railway 
+// Em vez da 3333, eu quero usar a variavel ambiente PORT e se ela não existir quero usar a porta 3333
+app.listen(process.env.PORT || 3333, () => {
   console.log("HTTP server running!");
 });
