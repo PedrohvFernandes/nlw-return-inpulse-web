5f52c100fbca459d40fe099f3c4b8aaf4baaacf0
Pedro Henrique Vieira Fernandes <72428350+PedrohvFernandes@users.noreply.github.com>
2022-05-07 20:58:46 -0300

    PrismaFeedbackRepository

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,20 +1,20 @@
-import express from "express";
-
-import { SubmitFeedbackUseCase } from "./use-cases/submit-feedback-use-case";
-import { NodemailerMailAdapter } from "./adapters/nodemailer/nodemailer-mail-adapter";
-import { PrismaFeedbackRepository } from "./repositories/prisma/prisma-feebacks-repository";
+import express from 'express';
+import { NodemailerMailAdapter } from './adapters/nodemailer/nodemailer-mail-adapter';
+import { PrismaFeedbackRepository } from './repositories/prisma/prisma-feebacks-repository';
+import { SubmitFeedbackUseCase } from './use-cases/submit-feedback-use-case';
 
 export const routes = express.Router();
-routes.post("/feedbacks", async (request, response) => {
-  const { type, comment, screenshot } = request.body;
 
-  try{
-    const prismaFeedbackRepository = new PrismaFeedbackRepository();
+routes.post('/feedbacks', async (req, res) => {
+  const { type, comment, screenshot } = req.body;
+
+  try {
+    const prismaFeedbacksRepository = new PrismaFeedbackRepository();
     const nodemailerMailAdapter = new NodemailerMailAdapter();
-    
+
     const submitFeedbackUseCase = new SubmitFeedbackUseCase(
-      prismaFeedbackRepository,
-      nodemailerMailAdapter
+      prismaFeedbacksRepository,
+      nodemailerMailAdapter,
     );
 
     await submitFeedbackUseCase.execute({
@@ -22,9 +22,11 @@ routes.post("/feedbacks", async (request, response) => {
       comment,
       screenshot,
     });
-    return response.status(201).send();
-} catch (error) {
-   console.log(error)
-    return response.status(500).send();
-}
+
+    return res.status(201).send();
+  } catch (err) {
+    console.log(err);
+
+    return res.status(500).send();
+  }
 });
